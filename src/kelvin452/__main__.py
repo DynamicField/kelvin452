2a8035fa2cce619b3c11e53dacf19f94b984cc29
FDGKLRTC <jambonfromagederussie@gmail.com>
2022-05-15 23:06:03 +0200

    j'ai mis p1ed_sprite et p10ed_sprite dans assets.py et les images des assets aussi

--- src/kelvin452/__main__.py
+++ src/kelvin452/__main__.py
@@ -28,11 +28,36 @@ class FireEntity(Entity):
         self.__sprite.rect.topleft = self.__x, self.__y # type: ignore
         self.__sprite.dirty = 1
 
+class piece1Entity(Entity):
+    def __init__(self, x, y):
+        super().__init__()
+        self.__x = x
+        self.__y = y
+        p1ed = pygame.transform.scale(all_assets.p1ed_sprite, (220, 180))
+        self.__sprite = make_sprite(p1ed, (x, y))
+
+    def _spawned(self):
+        self.show_sprite(self.__sprite)
+
+    def _tick(self):
+        self.__x += 500 * game.delta_time
+        self.__y += 50 * game.delta_time
+        if self.__x > game.viewport[0]:
+            self.__x = 0
+        if self.__y > game.viewport[1]:
+            self.__y = 0
+        self.__sprite.rect.topleft = self.__x, self.__y # type: ignore
+        self.__sprite.dirty = 1
+
+"""def game_start():
+    for i in range(100):
+        fire_entity = FireEntity(random.randint(0, 1280), random.randint(0, 720))
+        game.world.spawn_entity(fire_entity)"""
 
 def game_start():
     for i in range(100):
-        fire_entity = FireEntity(random.randint(0, 1280), random.randint(0, 720))
-        game.world.spawn_entity(fire_entity)
+        p1ed_entity = piece1Entity(random.randint(0, 1280), random.randint(0, 720))
+        game.world.spawn_entity(p1ed_entity)
 
 
 def launch_game():
