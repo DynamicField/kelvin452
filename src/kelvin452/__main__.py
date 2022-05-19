d3af012840f799787030a778ff43e7728972d60c
FDGKLRTC <jambonfromagederussie@gmail.com>
2022-05-19 16:53:22 +0200

    sans feu paske bug

--- src/kelvin452/__main__.py
+++ src/kelvin452/__main__.py
@@ -9,35 +9,25 @@ import random
 
 
 class FireEntity(Entity):
-    class FireEntity(Entity):
-        def __init__(self, x, y):
-            super().__init__()
-            self.__x = x
-            self.__y = y
-            self.position = [x, y]
-            huge_fire_sprite = pygame.transform.scale(all_assets.sprite("fire.png"), (90, 90))
-            self.__sprite = make_sprite(huge_fire_sprite, (x, y))
+    def __init__(self, x, y):
+        super().__init__()
+        self.__x = x
+        self.__y = y
+        huge_fire_sprite = pygame.transform.scale(all_assets.fire_sprite, (220, 180))
+        self.__sprite = make_sprite(huge_fire_sprite, (x, y))
 
-        def add_y(self, add):  # add is the value we add in position y value, for example y == 10, add_y(10) put y at 20
-            self.__y += add
+    def _spawned(self):
+        self.show_sprite(self.__sprite)
 
-        def _spawned(self):
-            self.show_sprite(self.__sprite)
-
-        def _tick(self):
-            if game.input.is_key_down(pygame.K_DOWN):
-                if self.__y + 100 <= 720:
-                    self.add_y(10)
-            if game.input.is_key_down(pygame.K_UP):
-                if self.__y - 10 >= 0:
-                    self.add_y(-10)
-            self.__sprite.rect.topleft = self.__x, self.__y  # type: ignore
-            self.__sprite.dirty = 1
-            self.position[0] = self.__x
-            self.position[1] = self.__y
-
-        def get_position(self):
-            return self.position
+    def _tick(self):
+        self.__x += 500 * game.delta_time
+        self.__y += 50 * game.delta_time
+        if self.__x > game.viewport[0]:
+            self.__x = 0
+        if self.__y > game.viewport[1]:
+            self.__y = 0
+        self.__sprite.rect.topleft = self.__x, self.__y # type: ignore
+        self.__sprite.dirty = 1
 
 class Piece1Entity(Entity):
     def __init__(self, x, y):
