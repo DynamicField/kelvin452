413d7796ec73b317003350104e42b1af071d1911
Nathanlennon <nathaanbias@gmail.com>
2022-05-19 16:20:49 +0200

    chuech

--- src/kelvin452/game/__main__.py
+++ src/kelvin452/game/__main__.py
@@ -16,9 +16,6 @@ class FireEntity(Entity):
         huge_fire_sprite = pygame.transform.scale(all_assets.sprite("fire.png"), (90, 90))
         self.__sprite = make_sprite(huge_fire_sprite, (x, y))
 
-    def add_x(self, add):  # add is the value we add in position x value, for example x == 10, add_x(10) put x at 20
-        self.__x += add
-
     def add_y(self, add):  # add is the value we add in position y value, for example y == 10, add_y(10) put y at 20
         self.__y += add
 
@@ -26,19 +23,19 @@ class FireEntity(Entity):
         self.show_sprite(self.__sprite)
 
     def _tick(self):
-        if game.input.is_key_down(pygame.K_RIGHT):
-            if self.__x + 256 <= 1280:
-                self.add_x(10)
-        if game.input.is_key_down(pygame.K_LEFT):
-            if self.__x - 974 >= 0:
-                self.add_x(-10)
         if game.input.is_key_down(pygame.K_DOWN):
-            self.add_y(10)
+            if self.__y + 100 <= 720:
+                self.add_y(10)
         if game.input.is_key_down(pygame.K_UP):
             if self.__y - 10 >= 0:
                 self.add_y(-10)
         self.__sprite.rect.topleft = self.__x, self.__y  # type: ignore
         self.__sprite.dirty = 1
+        self.position[0] = self.__x
+        self.position[1] = self.__y
+
+    def get_position(self):
+        return self.position
 
 
 def game_start():
