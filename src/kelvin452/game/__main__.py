dd077732346b24dcb5b9008e325a96fa6e634980
Nathanlennon <nathaanbias@gmail.com>
2022-05-20 14:34:46 +0200

    chuech

--- src/kelvin452/game/__main__.py
+++ src/kelvin452/game/__main__.py
@@ -1,68 +1,47 @@
 import pygame.transform
 
-from kelvin452.engine.systems.rendering import make_sprite
-from kelvin452.engine.systems.world import Entity
-from kelvin452.engine.game import game
-from kelvin452.engine.assets import all_assets
+from kelvin452.engine import *
 
 
 class FireEntity(Entity):
     def __init__(self, x, y):
         super().__init__()
-        self.__x = x
-        self.__y = y
-        self.position = [x, y]
+        self.position = Vector2(x, y)
         self.shoot_cooldown = 1
         self.timer = 0
-        huge_fire_sprite = pygame.transform.scale(all_assets.sprite("fire.png"), (90, 90))
-        self.__sprite = make_sprite(huge_fire_sprite, (x, y))
+        huge_fire_sprite = pygame.transform.scale(assets.sprite("fire.png"), (90, 90))
+        self.__sprite = self.attach_component(make_sprite(huge_fire_sprite, (x, y)))
 
     def add_y(self, add):  # add is the value we add in position y value, for example y == 10, add_y(10) put y at 20
-        self.__y += add
-
-    def _spawned(self):
-        self.show_sprite(self.__sprite)
+        self.position.y += add
 
     def _tick(self):
         self.timer -= game.delta_time
         if pygame.mouse.get_pressed()[0] or game.input.is_key_down(pygame.K_SPACE):
             if self.timer <= 0:
-                dragon_entity = DragonEntity(self.position[0], self.position[1] + 30)
+                dragon_entity = DragonEntity(self.position.x, self.position.y + 30)
                 game.world.spawn_entity(dragon_entity)
                 self.timer = self.shoot_cooldown
 
         if game.input.is_key_down(pygame.K_DOWN):
-            if self.__y + 100 <= 720:
+            if self.position.y + 100 <= 720:
                 self.add_y(10)
         if game.input.is_key_down(pygame.K_UP):
-            if self.__y - 10 >= 0:
+            if self.position.y - 10 >= 0:
                 self.add_y(-10)
-        self.__sprite.rect.topleft = self.__x, self.__y  # type: ignore
-        self.__sprite.dirty = 1
-        self.position[0] = self.__x
-        self.position[1] = self.__y
-
-    def get_position(self):
-        return self.position
 
 
 class DragonEntity(Entity):
     def __init__(self, x, y):
         super().__init__()
-        self.__x = x
-        self.__y = y
-        huge_dragon_sprite = pygame.transform.scale(all_assets.sprite("dragon.png"), (60, 43))
-        self.__sprite = make_sprite(huge_dragon_sprite, (x, y))
-
-    def _spawned(self):
-        self.show_sprite(self.__sprite)
+        self.position = Vector2(x, y)
+        huge_dragon_sprite = pygame.transform.scale(assets.sprite("dragon.png"), (60, 43))
+        self.__sprite = self.attach_component(make_sprite(huge_dragon_sprite, (x, y)))
 
     def _tick(self):
-        self.__x -= 10
-        if self.__x < 0:
+        self.position.x -= 10
+        if self.position.x < 0:
             game.world.destroy_entity(self)
-        self.__sprite.rect.topleft = self.__x, self.__y  # type: ignore
-        self.__sprite.dirty = 1
 
 
 def game_start():
