7accb61242ef419934277dc563359c03bffe1669
Nathanlennon <nathaanbias@gmail.com>
2022-05-20 19:05:30 +0200

    chuech

--- src/kelvin452/__main__.py
+++ src/kelvin452/__main__.py
@@ -1,55 +1,28 @@
 import pygame.transform
 from pygame import Rect
 
-from kelvin452.systems.rendering import make_sprite
-from kelvin452.systems.world import Entity
-from kelvin452.game import game
-from kelvin452.assets import all_assets
+from kelvin452.engine import *
+
 import random
 
-
-class FireEntity(Entity):
-    def __init__(self, x, y):
-        super().__init__()
-        self.__x = x
-        self.__y = y
-        huge_fire_sprite = pygame.transform.scale(all_assets.fire_sprite, (220, 180))
-        self.__sprite = make_sprite(huge_fire_sprite, (x, y))
-
-    def _spawned(self):
-        self.show_sprite(self.__sprite)
-
-    def _tick(self):
-        self.__x += 500 * game.delta_time
-        self.__y += 50 * game.delta_time
-        if self.__x > game.viewport[0]:
-            self.__x = 0
-        if self.__y > game.viewport[1]:
-            self.__y = 0
-        self.__sprite.rect.topleft = self.__x, self.__y # type: ignore
-        self.__sprite.dirty = 1
 
 class Piece1Entity(Entity):
     def __init__(self, x, y):
         super().__init__()
-        self.__x = x
-        self.__y = y
-        a = random.randint(32,64)
-        p1ed = pygame.transform.scale(all_assets.p1ed_sprite, (a,a))
-        self.__sprite = make_sprite(p1ed, (x, y))
+        self.position = Vector2(x, y)
+
+        a = random.randint(32, 64)
+        p1ed = pygame.transform.scale(assets.sprite("p1ed.png"), (a, a))
+        self.__sprite = self.attach_component(make_sprite(p1ed, (x, y)))
         self.p1ed_rect = self.__sprite.rect
 
-    def _spawned(self):
-        self.show_sprite(self.__sprite)
-
     def _tick(self):
-        self.__x += 200 * game.delta_time
-        if self.__x > 1000:
-            self.__x = 1000
-        self.__sprite.rect.topleft = self.__x, self.__y # type: ignore
-        self.__sprite.dirty = 1
+        self.position.x += 200 * game.delta_time
+        if self.position.x > 1000:
+            self.position.x = 1000
         """if self.p1ed_rect.colliderect(Rect(1000,0,100,900)):
             game.world.destroy_entity(self)"""
+
 
 class Entity_spawn(Entity):
     def __init__(self):
@@ -63,7 +36,7 @@ class Entity_spawn(Entity):
         if self.compte2 == True:
             self.compteur2(0.4)
 
-    def compteur(self,temps):
+    def compteur(self, temps):
         z = 0
         b = [120, 190, 260, 330, 400]
         self.compteurino -= game.delta_time
@@ -80,7 +53,7 @@ class Entity_spawn(Entity):
     def compteur2(self, temps):
         z = 0
         b = [120, 190, 260, 330, 400]
-        p10ed_number = random.randint(1,5)
+        p10ed_number = random.randint(1, 5)
         self.compteuridos -= game.delta_time
         if self.compteuridos <= 0:
             for i in range(p10ed_number):
@@ -92,33 +65,28 @@ class Entity_spawn(Entity):
             self.compteuridos = temps
             self.compte2 = False
 
+
 class Piece10Entity(Entity):
     def __init__(self, x, y):
         super().__init__()
-        self.__x = x
-        self.__y = y
+        self.position.x = x
+        self.position.y = y
         a = random.randint(32, 64)
-        p10ed = pygame.transform.scale(all_assets.p10ed_sprite, (a,a))
-        self.__sprite = make_sprite(p10ed, (x, y))
+        p10ed = pygame.transform.scale(assets.sprite("p10ed.png"), (a, a))
+        self.__sprite = self.attach_component(make_sprite(p10ed, (x, y)))
         self.p10ed_rect = self.__sprite.rect
 
-    def _spawned(self):
-        self.show_sprite(self.__sprite)
-
     def _tick(self):
-        self.__x += 200 * game.delta_time
-        if self.__x > game.viewport[0] - 700:
-            self.__x = game.viewport[0] - 700
-        self.__sprite.rect.topleft = self.__x, self.__y  # type: ignore
-        self.__sprite.dirty = 1
-        if self.p10ed_rect.colliderect(Rect(750, 0, 100, 700)):
-            game.world.destroy_entity(self)
+        self.position.x += 200 * game.delta_time
+        if self.position.x > game.viewport[0] - 700:
+            self.position.x = game.viewport[0] - 700
+
 
 class ProjEntity(Entity):
-    def __init__(self,x,y):
+    def __init__(self, x, y):
         super().__init__()
-        self.__x = x
-        self.__y = y
+        self.position.x = x
+        self.position.y = y
 
 
 def game_start():
@@ -138,6 +106,7 @@ def launch_game():
     game.initialize_game()
     game.on_start(game_start)
     game.start()
+
 
 if __name__ == "__main__":
     launch_game()
