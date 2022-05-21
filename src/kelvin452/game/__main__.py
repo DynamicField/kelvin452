087b7bc623a068b7f5fa684a96eb716d6cf4c6b4
Nathanlennon <nathaanbias@gmail.com>
2022-05-21 19:20:23 +0200

    aled

--- src/kelvin452/game/__main__.py
+++ src/kelvin452/game/__main__.py
@@ -50,6 +50,103 @@ class DragonEntity(Entity):
             game.world.destroy_entity(self)
 
 
+class Piece1Entity(Entity,ReactsToCollisions):
+    def __init__(self, x, y):
+        super().__init__()
+        self.position = Vector2(x, y)
+        a = random.randint(32, 64)
+        p1ed = pygame.transform.scale(assets.sprite("p1ed.png"), (a, a))
+        self.__sprite = self.attach_component(make_sprite(p1ed, (x, y)))
+        self.__collision = self.attach_component(CollisionHitBox(follow_sprite_rect=True, draw_box=True))
+
+    def _tick(self):
+        self.position.x += 200 * game.delta_time
+        if self.position.x > 1000:
+            self.position.x = 1000
+
+    def _on_collide(self, other: Entity):
+        if isinstance(other, DragonEntity):
+            game.world.destroy_entity(self)
+
+
+
+
+class Entity_spawn(Entity):
+    def __init__(self):
+        super().__init__()
+        self.compteurino = 10
+        self.compteuridos = 0.4
+        self.compte2 = False
+
+    def _tick(self):
+        self.compteur(10)
+        if self.compte2 == True:
+            self.compteur2(0.4)
+
+    def compteur(self, temps):
+        z = 0
+        b = [250, 340, 430]
+        self.compteurino -= game.delta_time
+        if self.compteurino <= 0:
+            self.compte2 = True
+            for i in range(3):
+                if z >= 5:
+                    z = 0
+                p1ed_entity = Piece1Entity(0, b[z])
+                game.world.spawn_entity(p1ed_entity)
+                z += 1
+            self.compteurino = temps
+
+    def compteur2(self, temps):
+        z = 0
+        b = [250, 340, 430]
+        p10ed_number = random.randint(1, 3)
+        self.compteuridos -= game.delta_time
+        if self.compteuridos <= 0:
+            for i in range(p10ed_number):
+                if z >= 3:
+                    z = 0
+                p10ed_entity = Piece10Entity(0, b[z])
+                game.world.spawn_entity(p10ed_entity)
+                z += 1
+            self.compteuridos = temps
+            self.compte2 = False
+
+
+class Piece10Entity(Entity,ReactsToCollisions):
+    def __init__(self, x, y):
+        super().__init__()
+        self.position = Vector2(x, y)
+        self.compteurProj = random.randint(4,6)
+        self.compteurProjRes = self.compteurProj
+        a = random.randint(32, 64)
+        p10ed = pygame.transform.scale(assets.sprite("p10ed.png"), (a, a))
+        self.__sprite = self.attach_component(make_sprite(p10ed, (x, y)))
+        self.__collision = self.attach_component(CollisionHitBox(follow_sprite_rect=True, draw_box=True))
+
+    def _tick(self):
+        if self.position.x >= 575:
+            self.compteurProj -= game.delta_time
+            if self.compteurProj <= 0:
+                proj_entity = ProjEntity(self.position.x, self.position.y)
+                game.world.spawn_entity(proj_entity)
+                self.compteurProj = self.compteurProjRes
+        self.position.x += 200 * game.delta_time
+        if self.position.x > 580:
+            self.position.x = 580
+
+
+class ProjEntity(Entity,ReactsToCollisions):
+    def __init__(self, x, y):
+        super().__init__()
+        self.position = Vector2(x, y)
+        self.__launched = False
+        proj = pygame.transform.scale(assets.sprite("projectile.png"), (100,100))
+        self.__sprite = self.attach_component(make_sprite(proj, (x, y)))
+        self.__collision = self.attach_component(CollisionHitBox(follow_sprite_rect=True, draw_box=True))
+
+    def _tick(self):
+        self.position.x += 10
 
 
 def game_start():
@@ -57,6 +154,17 @@ def game_start():
     game.renderer.background = assets.background("background.png")
     fire_entity = FireEntity(1024, 315)
     game.world.spawn_entity(fire_entity)
+    # partie d'alix en dessous
+    z = 0
+    b = [250, 340, 430]
+    spawner = Entity_spawn()
+    game.world.spawn_entity(spawner)
+    for i in range(3):
+        if z >= 3:
+            z = 0
+        p1ed_entity = Piece1Entity(0, b[z])
+        game.world.spawn_entity(p1ed_entity)
+        z += 1
 
 
 
