7e864d86cd549449320a2bb12103208893b660e8
Nathanlennon <nathaanbias@gmail.com>
2022-05-21 18:31:35 +0200

    chuech

--- src/kelvin452/game/__main__.py
+++ src/kelvin452/game/__main__.py
@@ -12,6 +12,7 @@ class FireEntity(Entity):
         self.timer = 0
         huge_fire_sprite = pygame.transform.scale(assets.sprite("fire.png"), (90, 90))
         self.__sprite = self.attach_component(make_sprite(huge_fire_sprite, (x, y)))
+        self.__collision = self.attach_component(CollisionHitBox(follow_sprite_rect=True, draw_box=True))
 
     def add_y(self, add):  # add is the value we add in position y value, for example y == 10, add_y(10) put y at 20
         self.position.y += add
@@ -25,10 +26,10 @@ class FireEntity(Entity):
                 self.timer = self.shoot_cooldown
 
         if game.input.is_key_down(pygame.K_DOWN):
-            if self.position.y + 100 <= 720:
+            if self.position.y + 100 <= 602:
                 self.add_y(10)
         if game.input.is_key_down(pygame.K_UP):
-            if self.position.y - 10 >= 0:
+            if self.position.y - 10 >= 100:
                 self.add_y(-10)
 
 
@@ -38,6 +39,10 @@ class DragonEntity(Entity):
         self.position = Vector2(x, y)
         huge_dragon_sprite = pygame.transform.scale(assets.sprite("dragon.png"), (60, 43))
         self.__sprite = self.attach_component(make_sprite(huge_dragon_sprite, (x, y)))
+        self.__collision = self.attach_component(CollisionHitBox(follow_sprite_rect=True, draw_box=True))
+
+    def _on_collide(self, other: Entity):
+        print("oskour la collision vers " + str(other))
 
     def _tick(self):
         self.position.x -= 10
