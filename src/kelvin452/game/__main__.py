1078288165efee1bf1292e439a340ae7740d2049
Nathanlennon <nathaanbias@gmail.com>
2022-05-21 18:31:35 +0200

    chuech

--- src/kelvin452/game/__main__.py
+++ src/kelvin452/game/__main__.py
@@ -4,7 +4,7 @@ from kelvin452.engine import *
 import random
 
 
-class FireEntity(Entity,ReactsToCollisions):
+class FireEntity(Entity):
     def __init__(self, x, y):
         super().__init__()
         self.position = Vector2(x, y)
@@ -12,7 +12,7 @@ class FireEntity(Entity,ReactsToCollisions):
         self.timer = 0
         huge_fire_sprite = pygame.transform.scale(assets.sprite("fire.png"), (90, 90))
         self.__sprite = self.attach_component(make_sprite(huge_fire_sprite, (x, y)))
-        self.__collision = self.attach_component(CollisionHitBox(follow_sprite_rect=True, draw_box=False))
+        self.__collision = self.attach_component(CollisionHitBox(follow_sprite_rect=True, draw_box=True))
 
     def add_y(self, add):  # add is the value we add in position y value, for example y == 10, add_y(10) put y at 20
         self.position.y += add
@@ -26,29 +26,27 @@ class FireEntity(Entity,ReactsToCollisions):
                 self.timer = self.shoot_cooldown
 
         if game.input.is_key_down(pygame.K_DOWN):
-            if self.position.y + 100 <= 720:
+            if self.position.y + 100 <= 602:
                 self.add_y(10)
         if game.input.is_key_down(pygame.K_UP):
-            if self.position.y - 10 >= 0:
+            if self.position.y - 10 >= 100:
                 self.add_y(-10)
 
 
-class DragonEntity(Entity,ReactsToCollisions):
+class DragonEntity(Entity):
     def __init__(self, x, y):
         super().__init__()
         self.position = Vector2(x, y)
         huge_dragon_sprite = pygame.transform.scale(assets.sprite("dragon.png"), (60, 43))
         self.__sprite = self.attach_component(make_sprite(huge_dragon_sprite, (x, y)))
-        self.__collision = self.attach_component(CollisionHitBox(follow_sprite_rect=True, draw_box=False))
+        self.__collision = self.attach_component(CollisionHitBox(follow_sprite_rect=True, draw_box=True))
+
+    def _on_collide(self, other: Entity):
+        print("oskour la collision vers " + str(other))
 
     def _tick(self):
         self.position.x -= 10
         if self.position.x < 0:
-            game.world.destroy_entity(self)
-
-    def _on_collide(self, other: Entity):
-        if isinstance(other, Piece1Entity) or isinstance(other, Piece10Entity):
-            game.world.destroy_entity(other)
             game.world.destroy_entity(self)
 
 
@@ -56,15 +54,18 @@ class Piece1Entity(Entity):
     def __init__(self, x, y):
         super().__init__()
         self.position = Vector2(x, y)
+
         a = random.randint(32, 64)
         p1ed = pygame.transform.scale(assets.sprite("p1ed.png"), (a, a))
         self.__sprite = self.attach_component(make_sprite(p1ed, (x, y)))
-        self.__collision = self.attach_component(CollisionHitBox(follow_sprite_rect=True, draw_box=False))
+        self.p1ed_rect = self.__sprite.rect
 
     def _tick(self):
         self.position.x += 200 * game.delta_time
-        if self.position.x > 900:
-            self.position.x = 900
+        if self.position.x > 1000:
+            self.position.x = 1000
+        """if self.p1ed_rect.colliderect(Rect(1000,0,100,900)):
+            game.world.destroy_entity(self)"""
 
 
 class Entity_spawn(Entity):
@@ -81,11 +82,11 @@ class Entity_spawn(Entity):
 
     def compteur(self, temps):
         z = 0
-        b = [250, 340, 430]
+        b = [120, 190, 260, 330, 400]
         self.compteurino -= game.delta_time
         if self.compteurino <= 0:
             self.compte2 = True
-            for i in range(3):
+            for i in range(5):
                 if z >= 5:
                     z = 0
                 p1ed_entity = Piece1Entity(0, b[z])
@@ -95,12 +96,12 @@ class Entity_spawn(Entity):
 
     def compteur2(self, temps):
         z = 0
-        b = [250, 340, 430]
-        p10ed_number = random.randint(1, 3)
+        b = [120, 190, 260, 330, 400]
+        p10ed_number = random.randint(1, 5)
         self.compteuridos -= game.delta_time
         if self.compteuridos <= 0:
             for i in range(p10ed_number):
-                if z >= 3:
+                if z >= 5:
                     z = 0
                 p10ed_entity = Piece10Entity(0, b[z])
                 game.world.spawn_entity(p10ed_entity)
@@ -112,42 +113,24 @@ class Entity_spawn(Entity):
 class Piece10Entity(Entity):
     def __init__(self, x, y):
         super().__init__()
-        self.position = Vector2(x, y)
-        self.compteurProj = random.uniform(4,6)
-        self.compteurProjRes = self.compteurProj
+        self.position.x = x
+        self.position.y = y
         a = random.randint(32, 64)
         p10ed = pygame.transform.scale(assets.sprite("p10ed.png"), (a, a))
         self.__sprite = self.attach_component(make_sprite(p10ed, (x, y)))
-        self.__collision = self.attach_component(CollisionHitBox(follow_sprite_rect=True, draw_box=False))
+        self.p10ed_rect = self.__sprite.rect
 
     def _tick(self):
-        if self.position.x >= 575:
-            self.compteurProj -= game.delta_time
-            if self.compteurProj <= 0:
-                proj_entity = ProjEntity(self.position.x, self.position.y)
-                game.world.spawn_entity(proj_entity)
-                self.compteurProj = 0
         self.position.x += 200 * game.delta_time
-        if self.position.x > 580:
-            self.position.x = 580
+        if self.position.x > game.viewport[0] - 700:
+            self.position.x = game.viewport[0] - 700
 
 
-class ProjEntity(Entity, ReactsToCollisions):
+class ProjEntity(Entity):
     def __init__(self, x, y):
         super().__init__()
-        self.position = Vector2(x, y)
-        self.__launched = False
-        proj = pygame.transform.scale(assets.sprite("projectile.png"), (100,100))
-        self.__sprite = self.attach_component(make_sprite(proj, (x, y)))
-        self.__collision = self.attach_component(CollisionHitBox(follow_sprite_rect=True, draw_box=False))
-
-    def _tick(self):
-        self.position.x += 10
-
-    def _on_collide(self, other: Entity):
-        if isinstance(other, FireEntity):
-            game.world.destroy_entity(self)
-
+        self.position.x = x
+        self.position.y = y
 
 
 def game_start():
@@ -158,11 +141,11 @@ def game_start():
 
     # partie d'alix en dessous
     z = 0
-    b = [250, 340, 430]
+    b = [120, 190, 260, 330, 400]
     spawner = Entity_spawn()
     game.world.spawn_entity(spawner)
-    for i in range(3):
-        if z >= 3:
+    for i in range(5):
+        if z >= 5:
             z = 0
         p1ed_entity = Piece1Entity(0, b[z])
         game.world.spawn_entity(p1ed_entity)
