d807f756eb1e142b1facfb3951b09b6a6685567b
FDGKLRTC <jambonfromagederussie@gmail.com>
2022-05-21 20:52:31 +0200

    mis le game.delta_time dans le mouvement de ProjEntity (pour de vrai cette fois)

--- src/kelvin452/game/__main__.py
+++ src/kelvin452/game/__main__.py
@@ -143,7 +143,7 @@ class ProjEntity(Entity, ReactsToCollisions):
         self.__collision = self.attach_component(CollisionHitBox(follow_sprite_rect=True, draw_box=False))
 
     def _tick(self):
-        self.position.x += 10 * game.delta_time
+        self.position.x += 600 * game.delta_time
         if self.position.x > 1200:
             game.world.destroy_entity(self)
 
