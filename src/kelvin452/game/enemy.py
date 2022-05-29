3295f95fe40e5e09ac0c950118b81118ebadb04f
Nathanlennon <nathaanbias@gmail.com>
2022-05-29 10:34:45 +0200

    ajout d'un compteur pour les ennemis

--- src/kelvin452/game/enemy.py
+++ src/kelvin452/game/enemy.py
@@ -11,12 +11,12 @@ def add_enemy(value):
 
 
 class EnemyText(Entity):
-    def __init__(self, x=900, y=640):
+    def __init__(self, x=1161, y=612):
         super().__init__()
         self.position = Vector2(x, y)
         self.previous_enemy = -1
         self.survive_game_over = True
-        self.text_sprite = self.attach_component(KelvinSprite(pygame.Surface((0, y))))
+        self.text_sprite = self.attach_component(KelvinSprite(pygame.Surface((self.position.x, self.position.y))))
 
     def _tick(self):
         # Refaire le rendu du texte uniquement lorsque le enemy change
@@ -24,7 +24,5 @@ class EnemyText(Entity):
             self.text_sprite.image = default_font.render(f"ENEMY : {enemy}",
                                                          True,
                                                          (255, 255, 255))
-            # Centrer le enemy
-            self.position.x = (game.viewport.x - self.text_sprite.image.get_rect().width) / 2
             self.text_sprite.dirty = 1  # Mettre à jour le sprite
             self.previous_enemy = enemy
